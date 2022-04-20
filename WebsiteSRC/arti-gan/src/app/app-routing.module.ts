55c5bc2cfdba7839de3c6dd33b546f8bf9853a35
mobridge <mobridge@iu.edu>
2022-04-20 17:09:27 -0400

    website updated

--- WebsiteSRC/arti-gan/src/app/app-routing.module.ts
+++ WebsiteSRC/arti-gan/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: "home", component: HomeComponent}
+  { path: "home", component: HomeComponent},
+  { path: "gallery", component: GalleryComponent},
+  { path: "about-us", component: AboutUsComponent}
 ];
 
 @NgModule({
--- WebsiteSRC/arti-gan/src/app/app.module.ts
+++ WebsiteSRC/arti-gan/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutUsComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
